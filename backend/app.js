8788574a11662fc83a527a89c1b3cdee698be033
MyBook PRO K5 <nabilacaca.101099@gmail.com>
2025-04-12 23:56:09 +0800

    init backend structure + test route

---
+++ backend/app.js
@@ -0,0 +1,19 @@
+// app.js
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+// Tes route
+app.get('/api/health', (req, res) => {
+  res.json({ message: 'Server is running!' });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
